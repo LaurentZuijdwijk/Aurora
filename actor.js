770ae9a894943fa254f3c2f5057bfdde03c5cf0d
LaurentZuijdwijk <laurent.zuijdwijk@gmail.com>
2011-09-10 16:51:32 +0100

    changed path

--- actor.js
+++ actor.js
@@ -1,39 +1,57 @@
 /**
+ * Actorpool is mainly a work in progress. This will have multiple options like 
+ * fork/join etc.
  *
- * Code for the Actor. No need to change this code in normal use.
- *
  */
-
-
-/**
- * ID of this actor. 
- */
-var id;
-
-/**
- * The method used for receiving messages.
- */
-var receive;
-
-
-self.onmessage = function(event){
-	if(event.data.fn)
-	{
-		eval(event.data.fn);
-		receive = fn();
-		self.postMessage('receive method: ' + receive.toString());
+var ActorPool = {
+	i : 0,
+	getId : function(){
+		ActorPool.i++;
+		return ActorPool.i;
 	}
-	else if (event.data.id)
-	{
-		id = event.data.id;
-		self.postMessage('id = ' + id);		
-	}
-	else if(event.data.msg)
-	{
-		self.postMessage({id:id, msg:receive(event.data.msg)});		
-	}	
-	else
-	{
-		self.postMessage('unknown message');		
-	}	
+	
 }
+
+
+
+
+/**
+ * Create a new actor. 
+ *
+ */
+var Actor = function(){};
+
+Actor.prototype.init = function(){
+ 		//TODO: check if the browser supports web workers.
+		//if not, fallback to normal operation.
+		console.log('WebWorkers supprted: ' + !!window.Worker);
+ 		
+ 		this.worker = new Worker('./_actor.js');
+  		this.worker.onmessage = this._onmessage;
+ 			 
+ 		this.worker.id = ActorPool.getId();	
+ 		this.worker.postMessage({id:this.worker.id});
+};
+Actor.prototype._onmessage = function(e){
+ 		console.log(e.data);
+ 	},
+Actor.prototype.worker = '';
+
+/**
+ * Set the receive function to parse messages and do calculations based on these messages.
+ *
+ */
+Actor.prototype.receive = function(fn){
+ 		var fun  = fn.toString();
+		this.worker.postMessage({fn:'var fn =function (){return '+fun+'}'});
+};
+Actor.prototype.send = function(data){
+		this.queue.push(data);
+		this.msgId ++;
+		this.worker.postMessage({msg:data, msgId:this.msgId});
+};
+
+// Add custom queue, to be able to create work stealing actors & fork/join
+Actor.prototype.queue : [];
+Actor.prototype.msgId : 0;
+
