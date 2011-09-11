ddaf40ca91cf7b4a8bc1afee56d3cba9c3f10011
LaurentZuijdwijk <laurent.zuijdwijk@gmail.com>
2011-09-11 12:34:44 +0100

    Made a working example

--- src/actor.js
+++ src/actor.js
@@ -8,9 +8,11 @@ var ActorPool = {
 	getId : function(){
 		ActorPool.i++;
 		return ActorPool.i;
-	}
-	
+	}	
 }
+var ActorConfig = {
+	workerPath : '_actor.js'
+};
 
 
 
@@ -21,37 +23,41 @@ var ActorPool = {
  */
 var Actor = function(){};
 
-Actor.prototype.init = function(){
+Actor.prototype = {
+	start : function(){
  		//TODO: check if the browser supports web workers.
 		//if not, fallback to normal operation.
 		console.log('WebWorkers supprted: ' + !!window.Worker);
- 		
- 		this.worker = new Worker('./_actor.js');
-  		this.worker.onmessage = this._onmessage;
- 			 
- 		this.worker.id = ActorPool.getId();	
- 		this.worker.postMessage({id:this.worker.id});
-};
-Actor.prototype._onmessage = function(e){
+		try{ 
+			this.worker = new Worker(ActorConfig.workerPath);
+		}
+		catch(e){console.error(e);}
+ 		this.worker.onmessage = this._onmessage;
+  		this.worker.id = ActorPool.getId();	
+  		this.worker.postMessage({id:this.worker.id});
+},
+ 	_onmessage : function(e){
+ 		console.log('msg received')
  		console.log(e.data);
  	},
-Actor.prototype.worker = '';
+	worker : '',
 
 /**
  * Set the receive function to parse messages and do calculations based on these messages.
  *
  */
-Actor.prototype.receive = function(fn){
+	receive : function(fn){
  		var fun  = fn.toString();
 		this.worker.postMessage({fn:'var fn =function (){return '+fun+'}'});
-};
-Actor.prototype.send = function(data){
+	},
+	send : function(data){
 		this.queue.push(data);
-		this.msgId ++;
+		this.msgId++;
 		this.worker.postMessage({msg:data, msgId:this.msgId});
-};
+	}
+}
 
 // Add custom queue, to be able to create work stealing actors & fork/join
-Actor.prototype.queue : [];
-Actor.prototype.msgId : 0;
+Actor.prototype.queue = [];
+Actor.prototype.msgId = 0;
 
